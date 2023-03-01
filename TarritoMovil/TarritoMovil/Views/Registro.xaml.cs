f53f19b897c9b71853f26cc0743f55f1d545d538
FranciscoTamayo <115678953+FranciscoTamayo@users.noreply.github.com>
2023-03-01 12:01:47 -0700

    Registro Terminado
    ahora si

--- TarritoMovil/TarritoMovil/Views/Registro.xaml.cs
+++ TarritoMovil/TarritoMovil/Views/Registro.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,18 @@ namespace TarritoMovil.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Registro : ContentPage
     {
+        public ICommand IniciarSesion => new Command(IrIniciarSesion);
+
+        public void IrIniciarSesion()
+        {
+           
+            Navigation.PushAsync(new Login());
+        }
         public Registro()
         {
             InitializeComponent();
+            BindingContext=this; 
+
         }
     }
 }
