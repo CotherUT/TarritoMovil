75a375a9eb387dff99f731edd5460b02fefce8f1
AbrilUT <111263222+AbrilUT@users.noreply.github.com>
2023-03-01 12:07:46 -0700

    Login terminado
    se termino el login sin imagen

--- TarritoMovil/TarritoMovil/Views/Login.xaml.cs
+++ TarritoMovil/TarritoMovil/Views/Login.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,23 @@ namespace TarritoMovil.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        public ICommand Registrarse => new Command(IrRegistrarse);
+
+        public void IrRegistrarse()
+        {
+
+            Navigation.PushAsync(new MainPage()); //Borrar esto y usar el comentario de abajo ya que el login este terminado
+            //Navigation.PushAsync(new Login());
+        }
         public Login()
         {
             InitializeComponent();
+            BindingContext= this;
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+         
         }
     }
 }
