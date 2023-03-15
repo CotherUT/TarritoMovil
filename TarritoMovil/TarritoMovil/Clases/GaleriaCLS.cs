a45eac403cec36d7f4fa07a984dc825a1ee1e822
FranciscoTamayo <115678953+FranciscoTamayo@users.noreply.github.com>
2023-03-15 11:17:14 -0700

    AvanceDetalle

---
+++ TarritoMovil/TarritoMovil/Clases/GaleriaCLS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TarritoMovil.Clases
+{
+    internal class GaleriaCLS
+    {
+        public string titulo { get; set; }
+        public string imagen { get; set; }
+    }
+}
